0c2d2072eb88c3683e98161d3ce06873a01b1de4
micah <micah@glowcoil.com>
2021-01-30 22:49:17 -0600

    Elem::layout and Elem::render

--- src/lib.rs
+++ src/lib.rs
@@ -37,8 +37,34 @@ impl Node {
     }
 }
 
+pub struct Constraints {
+    pub min_width: f32,
+    pub min_height: f32,
+    pub max_width: f32,
+    pub max_height: f32,
+}
+
+pub struct Size {
+    pub width: f32,
+    pub height: f32,
+}
+
+impl Size {
+    pub fn new(width: f32, height: f32) -> Size {
+        Size { width, height }
+    }
+}
+
+pub struct DisplayList {}
+
 pub trait Elem {
     fn base(&self) -> &ElemBase;
 
     fn walk(&mut self, walker: &mut ElemWalker) {}
+
+    fn layout(&mut self, constraints: Constraints) -> Size {
+        Size::new(constraints.min_width, constraints.min_height)
+    }
+
+    fn render(&mut self, display_list: &mut DisplayList) {}
 }
